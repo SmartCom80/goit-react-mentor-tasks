77058ad9c15e39cdfecf559d47edf9294f93a32d
RomannaLG1 <melnykromasja@gmail.com>
2022-12-03 15:01:48 +0200

    add 1 task

---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,53 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardPoster,
+  CardText,
+  CardTitle,
+  CardFooter,
+  Tag,
+  Avatar,
+  UserBox,
+  UserInfo,
+  UserName,
+  Date,
+} from './BlogCard.styled';
+import {formatDate} from '../helper';
+
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  name,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster
+          src={poster}
+          alt={title}
+        />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>
+          {description}
+        </CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{formatDate(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
+};
